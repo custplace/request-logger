c049b0c525eeda0e3f4985b5df579bf8a4f9f03d
Simoo105 <mohamedes691@gmail.com>
2022-01-24 10:50:19 +0000

    Update afterMiddleware.php

--- src/Http/Middleware/afterMiddleware.php
+++ src/Http/Middleware/afterMiddleware.php
@@ -4,18 +4,20 @@ namespace Simo\requestLogger\Http\Middleware;
 
 use Closure;
 use Simo\requestLogger\Models\RequestLogModel;
+use Simo\requestLogger\Models\RequestLogModelMongo;
 
 class AfterMiddleware
 {
     public function handle($request, Closure $next)
     {
         $response = $next($request);
+        $db_name = config('database.default');
         // Save request end time
         $request->end = microtime(true);
-        $db_name = config('database.default');
-        $LogModel = new RequestLogModel;
-        $test = $LogModel->setConnection($db_name);
-        dd($test);
+        if($db_name === 'mongodb') {
+            RequestLogModelMongo::logRequest($request, $response);
+            return $response;
+        }
         RequestLogModel::logRequest($request, $response);
 
         return $response;
