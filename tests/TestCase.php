2faff64ac521816d09a4fd01307fd2acf1589dd4
Simoo105 <mohamedes691@gmail.com>
2022-01-26 21:48:25 +0000

    fixing some tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,9 +22,9 @@ class TestCase extends \Orchestra\Testbench\TestCase
   protected function getEnvironmentSetUp($app)
   {
     // perform environment setup
-    include_once __DIR__ . '/../database/migrations/create_logs_table.php';
+    include_once __DIR__ . '/../../database/migrations/create_logs_table.php';
 
     // run the up() method of that migration class
-    (new \CreatePostsTable)->up();
+    (new \CreateLogsTable)->up();
     }
 }
