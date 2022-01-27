957675e713d10dfc726535726d4f5c7aca174691
Simoo105 <mohamedes691@gmail.com>
2022-01-27 11:59:14 +0000

    fixing some duplicate code

--- src/Providers/RequestLogProvider.php
+++ src/Providers/RequestLogProvider.php
@@ -15,7 +15,7 @@ class requestLogProvider extends ServiceProvider
             __DIR__.'/../../config/requestLogConfig.php' => config_path('requestLogConfig.php'),
         ]);
         
-        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations/');
+        //$this->loadMigrationsFrom(__DIR__ . '/../../database/migrations/');
         $this->loadRoutesFrom(__DIR__.'/../../routes/RequestLogTestingRoutes.php');
         $this->app->make(\Illuminate\Contracts\Http\Kernel::class)
             ->pushMiddleware(RequestLogBeforeMiddleware::class)
