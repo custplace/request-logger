0a6a24b6a63aae5e8c0298f5f39ddfd388c292b7
Simoo105 <mohamedes691@gmail.com>
2022-02-03 22:25:36 +0000

    adding two more options

--- config/request-log.php
+++ config/request-log.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Str;
 return [
     'enabled' => true,
     'connection_name' => 'mongodb',// driver name
-    'collection_name' => 'requests_log'// table name
+    'collection_name' => 'requests_log',// table name
+    'file_path' => storage_path('logs/requestLogger.log'),
+    'uris_that_should_be_ignored' => [],
 ];
--- src/Http/Middleware/RequestLogAfterMiddleware.php
+++ src/Http/Middleware/RequestLogAfterMiddleware.php
@@ -4,6 +4,7 @@ namespace Custplace\requestLogger\Http\Middleware;
 
 use Closure;
 use Custplace\requestLogger\Models\RequestLog;
+use Illuminate\Support\Facades\Log;
 use DB;
 
 class RequestLogAfterMiddleware
@@ -13,13 +14,35 @@ class RequestLogAfterMiddleware
         $response = $next($request);
         // Save request end time
         $request->end = microtime(true);
-        try {
-            RequestLog::logRequest($request, $response);
+        $shouldBeIgnored = config('request-log.uris_that_should_be_ignored');
+        if(!in_array($request->path(), $shouldBeIgnored)) {
+            try {
+                RequestLog::logRequest($request, $response);
+                Log::build([
+                    'driver' => 'single',
+                    'path' => config('request-log.file_path')
+                ])->info($this->convert($request, $response));
+            }
+            catch(\Exception $e) {
+                report($e);
+            }
         }
-        catch(\Exception $e) {
-            report($e);
-        }
-
         return $response;
     }
+
+    public function convert($request, $response)
+    {
+        return json_encode([
+            'app '             => $request->getHost(),
+            'path'             => $request->path(),
+            'headers'        => $request->headers->all(),
+            'method'          => $request->getMethod(),
+            'request'          => $request->all(),
+            'response_code'   => $response->status(),
+            'response_content' => $response->getContent(),
+            'duration'         => $request->end - $request->start,
+            'ip'              => $request->getClientIp(),
+        ]);
+    }
+
 }
